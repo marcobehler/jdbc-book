1e9a0696ab7af291b0b15551b9c8955c9cdd2055
marco <marco@marcobehler.com>
2020-05-07 17:12:26 +0200

    DataSource stuff

--- src/main/java/com/jdbc/ApplicationV5.java
+++ src/main/java/com/jdbc/ApplicationV5.java
@@ -2,6 +2,7 @@ package com.jdbc;
 
 import org.h2.jdbcx.JdbcDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -9,13 +10,18 @@ public class ApplicationV5 {
 
     public static void main(String[] args) throws SQLException {
 
-        JdbcDataSource ds = new JdbcDataSource();
-        ds.setURL("jdbc:h2:~/mydatabase");
-        ds.setUser("sa");
-        ds.setPassword("s3cr3tPassword");
+        DataSource ds = createDataSource();
 
         try (Connection connection = ds.getConnection()) {
             System.out.println("connection.isValid(0) = " + connection.isValid(0));
         }
+    }
+
+    private static DataSource createDataSource() {
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:~/mydatabase");
+        ds.setUser("sa");
+        ds.setPassword("s3cr3tPassword");
+        return ds;
     }
 }
