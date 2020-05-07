afa1e9d38302150119dec24fceb59367aaa5cf6d
marco <marco@marcobehler.com>
2020-05-07 13:55:22 +0200

    Added Password Examle

---
+++ src/main/java/com/jdbc/ApplicationV2.java
@@ -0,0 +1,15 @@
+package com.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ApplicationV2 {
+
+    public static void main(String[] args) throws SQLException {
+
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:", "sa", "sa")) {
+            System.out.println("connection.isValid(0) = " + connection.isValid(0));
+        }
+    }
+}
