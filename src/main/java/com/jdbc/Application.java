c80d7c80fe8d8f5ed7ae926d45c1a4b24a70a02a
marco <marco@marcobehler.com>
2020-05-07 11:49:50 +0200

    Initial Application class

--- src/main/java/com/jdbc/Application.java
+++ src/main/java/com/jdbc/Application.java
@@ -1,8 +1,15 @@
 package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:")) {
+            System.out.println("connection.isValid(0) = " + connection.isValid(0));
+        }
     }
 }
