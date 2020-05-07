dd2a25def36f62f97c747848061b82df2ed8e8a3
marco <marco@marcobehler.com>
2020-05-07 16:53:50 +0200

    Wrong username pass

--- src/main/java/com/jdbc/ApplicationV3.java
+++ src/main/java/com/jdbc/ApplicationV3.java
@@ -9,7 +9,7 @@ public class ApplicationV3 {
     public static void main(String[] args) throws SQLException {
 
         try (Connection connection = DriverManager
-                .getConnection("jdbc:h2:~/mydatabase", "sa", "someOtherPassword")) {
+                .getConnection("jdbc:h2:~/mydatabase", "sa", "s3cr3tPassword")) {
             System.out.println("connection.isValid(0) = " + connection.isValid(0));
         }
     }
