be6021842072263ec907666e65f8481618e56067
marco <marco@marcobehler.com>
2020-05-07 16:48:09 +0200

    File- based database

--- src/main/java/com/jdbc/ApplicationV3.java
+++ src/main/java/com/jdbc/ApplicationV3.java
@@ -8,7 +8,8 @@ public class ApplicationV3 {
 
     public static void main(String[] args) throws SQLException {
 
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:~/mydatabase", "sa", "somePassword")) {
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:h2:~/mydatabase", "sa", "someOtherPassword")) {
             System.out.println("connection.isValid(0) = " + connection.isValid(0));
         }
     }
