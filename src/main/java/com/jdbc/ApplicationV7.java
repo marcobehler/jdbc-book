8da6c4ce2d4f2632583474ec06b85d63793229b9
marco <marco@marcobehler.com>
2020-05-08 14:56:23 +0200

    Connection pooling

---
+++ src/main/java/com/jdbc/ApplicationV7.java
@@ -0,0 +1,27 @@
+package com.jdbc;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ApplicationV7 {
+
+    public static void main(String[] args) throws SQLException {
+
+        DataSource ds = createDataSource();
+
+        try (Connection connection = ds.getConnection()) {
+            System.out.println("connection.isValid(0) = " + connection.isValid(0));
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema.sql");
+        ds.setUsername("sa");
+        ds.setPassword("s3cr3tPassword");
+        return ds;
+    }
+}
