3c6f35ca479211560ef242dc16d7e4da83a4057e
marco <marco@marcobehler.com>
2020-05-08 18:35:01 +0200

    ApplicationV11

---
+++ src/main/java/com/jdbc/ApplicationV11.java
@@ -0,0 +1,49 @@
+package com.jdbc;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class ApplicationV11 {
+
+    public static void main(String[] args) throws SQLException {
+
+        DataSource ds = createDataSource();
+
+        try (Connection connection = ds.getConnection()) {
+
+            try (PreparedStatement stmt = connection.prepareStatement("update users set first_name = concat(first_name,?) where id > ? ")) {
+
+                stmt.setString(1,"-yay!");
+                stmt.setInt(2,5);
+                int updatedRows = stmt.executeUpdate();
+                System.out.println("I just updated " + updatedRows + " rows");
+            }
+
+            try (PreparedStatement stmt = connection.prepareStatement("select * from users")) {
+
+                ResultSet resultSet = stmt.executeQuery();
+                while (resultSet.next()) {
+                    Integer id = resultSet.getInt("id");
+                    String firstName = resultSet.getString("first_name");
+                    String lastName = resultSet.getString("last_name");
+                    LocalDateTime registrationDate = resultSet.getObject("registration_date", LocalDateTime.class);
+                    System.out.println("Found user: " + id + " | " +  firstName + " | " + lastName + " | " + registrationDate);
+                }
+            }
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setUsername("sa");
+        ds.setPassword("s3cr3tPassword");
+        return ds;
+    }
+}
