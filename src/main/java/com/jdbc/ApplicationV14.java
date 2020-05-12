c7fbb39f187b2b754d131f5701eb4147ec3bbe31
marco <marco@marcobehler.com>
2020-05-12 21:13:32 +0200

    schema.sql updated

---
+++ src/main/java/com/jdbc/ApplicationV14.java
@@ -0,0 +1,49 @@
+package com.jdbc;
+
+import com.zaxxer.hikari.HikariDataSource;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ApplicationV14 {
+
+    public static void main(String[] args) throws SQLException {
+
+        DataSource ds = createDataSource();
+
+        try (Connection connection = ds.getConnection()) {
+
+            try (PreparedStatement stmt = connection.prepareStatement("select * from users where id > ?")) {
+                stmt.setInt(1,5);
+
+                ResultSet resultSet = stmt.executeQuery();
+                while (resultSet.next()) {
+                    // do something useful here.
+                    System.out.println(resultSet.getInt("id"));
+                }
+
+            }
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setUsername("sa");
+        ds.setPassword("s3cr3tPassword");
+
+        DataSource dataSource =
+                ProxyDataSourceBuilder.create(ds)  // pass original datasource
+                        .logQueryToSysOut()
+                        // .logQueryByJUL()
+                        //.logQueryBySlf4j()
+                        //.logQueryByCommons()
+                        .build();
+
+        return dataSource;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,4 +3,15 @@ create table if not exists users (
    first_name varchar(255),
    last_name varchar(255),
    registration_date datetime
-)
+);
+
+alter table users add column IF NOT EXISTS balance int default 100;
+
+create table if not exists transactions (
+     id identity not null primary key,
+     sender int not null,
+     receiver int not null,
+     amount int not null,
+     foreign key (sender) references users(id),
+     foreign key (receiver) references users(id)
+);
