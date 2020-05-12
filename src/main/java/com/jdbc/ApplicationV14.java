b88e5a69d316989f51b38d9e5c8cd362ae9bf2a4
marco <marco@marcobehler.com>
2020-05-12 21:31:41 +0200

    applicataionv14

--- src/main/java/com/jdbc/ApplicationV14.java
+++ src/main/java/com/jdbc/ApplicationV14.java
@@ -4,29 +4,62 @@ import com.zaxxer.hikari.HikariDataSource;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class ApplicationV14 {
 
     public static void main(String[] args) throws SQLException {
+        // database transactions
 
         DataSource ds = createDataSource();
 
         try (Connection connection = ds.getConnection()) {
+            int senderId = createUser(connection);
+            int receiverId = createUser(connection);
+            int transactionId = sendMoney(connection, senderId, receiverId, 50);
+            System.out.println("Created users with ids => " + Arrays.toString(new int[]{senderId, receiverId}) + " and transaction with id = " + transactionId);
+        }
+    }
 
-            try (PreparedStatement stmt = connection.prepareStatement("select * from users where id > ?")) {
-                stmt.setInt(1,5);
+    private static int createUser(Connection connection) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement("insert into users (first_name, last_name, registration_date) values (?,?,?)"
+                , Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, "[Some FirstName]");
+            stmt.setString(2, "[Some LastName]");
+            stmt.setObject(3, LocalDateTime.now());
+            stmt.executeUpdate();
 
-                ResultSet resultSet = stmt.executeQuery();
-                while (resultSet.next()) {
-                    // do something useful here.
-                    System.out.println(resultSet.getInt("id"));
-                }
+            final ResultSet keysResultSet = stmt.getGeneratedKeys();
+            keysResultSet.next();
+            return keysResultSet.getInt(1);
+        }
+    }
 
-            }
+    private static int sendMoney(Connection connection, int senderId, int receiverId, int amount) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement("update users set balance = (balance - ?) where id = ?")) {
+            stmt.setInt(1, amount);
+            stmt.setInt(2, senderId);
+            stmt.executeUpdate();
+        }
+
+        try (PreparedStatement stmt = connection.prepareStatement("update users set balance = (balance + ?) where id = ?")) {
+            stmt.setInt(1, amount);
+            stmt.setInt(2, receiverId);
+            stmt.executeUpdate();
+        }
+
+        try (PreparedStatement stmt = connection.prepareStatement("insert into transactions (sender, receiver, amount) values (?,?,?)"
+                , Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, senderId);
+            stmt.setInt(2, receiverId);
+            stmt.setInt(3, amount);
+            stmt.executeUpdate();
+
+            final ResultSet keysResultSet = stmt.getGeneratedKeys();
+            keysResultSet.next();
+            return keysResultSet.getInt(1);
         }
     }
 
