c81bc5ba95a1bcb0511655b26cdb988c4978cb62
marco <marco@marcobehler.com>
2020-05-18 09:57:29 +0200

    Reformatting Code

--- src/main/java/com/jdbc/ApplicationV4.java
+++ src/main/java/com/jdbc/ApplicationV4.java
@@ -9,7 +9,8 @@ public class ApplicationV4 {
     public static void main(String[] args) throws SQLException {
 
         try (Connection connection = DriverManager
-                .getConnection("jdbc:h2:~/mydatabase", "sa", "definitelySomeOtherPassword")) {
+                .getConnection("jdbc:h2:~/mydatabase", "sa",
+                        "definitelySomeOtherPassword")) {
             System.out.println("connection.isValid(0) = " + connection.isValid(0));
         }
     }
