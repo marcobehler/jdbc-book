47caeef59ca249bc4311fee6c228c204871136c5
marco <marco@marcobehler.com>
2020-05-18 10:28:45 +0200

    V11 reformatted

--- src/main/java/com/jdbc/ApplicationV11.java
+++ src/main/java/com/jdbc/ApplicationV11.java
@@ -17,25 +17,29 @@ public class ApplicationV11 {
 
         try (Connection connection = ds.getConnection()) {
 
-            try (PreparedStatement stmt = connection.prepareStatement("update users set first_name = concat(first_name,?) where id > ? ")) {
+            try (PreparedStatement stmt = connection.prepareStatement("update " +
+                    "users set first_name = concat(first_name,?) where id > ? ")) {
 
-                stmt.setString(1,"-yay!");
-                stmt.setInt(2,5);
+                stmt.setString(1, "-yay!");
+                stmt.setInt(2, 5);
 
                 int updatedRows = stmt.executeUpdate();
 
                 System.out.println("I just updated " + updatedRows + " rows");
             }
 
-            try (PreparedStatement stmt = connection.prepareStatement("select * from users")) {
+            try (PreparedStatement stmt = connection
+                    .prepareStatement("select * " + "from users")) {
 
                 ResultSet resultSet = stmt.executeQuery();
                 while (resultSet.next()) {
                     Integer id = resultSet.getInt("id");
                     String firstName = resultSet.getString("first_name");
                     String lastName = resultSet.getString("last_name");
-                    LocalDateTime registrationDate = resultSet.getObject("registration_date", LocalDateTime.class);
-                    System.out.println("Found user: " + id + " | " +  firstName + " | " + lastName + " | " + registrationDate);
+                    LocalDateTime registrationDate = resultSet.getObject(
+                            "registration_date", LocalDateTime.class);
+                    System.out.println("Found user: " + id + " | " + firstName +
+                            " | " + lastName + " | " + registrationDate);
                 }
             }
         }
@@ -43,7 +47,8 @@ public class ApplicationV11 {
 
     private static DataSource createDataSource() {
         HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema" +
+                ".sql'");
         ds.setUsername("sa");
         ds.setPassword("s3cr3tPassword");
         return ds;
