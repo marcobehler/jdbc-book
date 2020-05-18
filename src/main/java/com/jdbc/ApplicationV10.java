8d3a3068fbb38a82a0974701b77756feb32d5353
marco <marco@marcobehler.com>
2020-05-18 10:02:29 +0200

    V10 reformatted

--- src/main/java/com/jdbc/ApplicationV10.java
+++ src/main/java/com/jdbc/ApplicationV10.java
@@ -16,16 +16,19 @@ public class ApplicationV10 {
 
         try (Connection connection = ds.getConnection()) {
 
-            try (PreparedStatement stmt = connection.prepareStatement("select * from users")) {
+            try (PreparedStatement stmt = connection.prepareStatement("select * " +
+                    "from users")) {
 
                 ResultSet resultSet = stmt.executeQuery();
                 while (resultSet.next()) {
                     int id = resultSet.getInt("id");
                     String firstName = resultSet.getString("first_name");
                     String lastName = resultSet.getString("last_name");
-                    LocalDateTime registrationDate = resultSet.getObject("registration_date", LocalDateTime.class);
+                    LocalDateTime registrationDate = resultSet.getObject(
+                            "registration_date", LocalDateTime.class);
 
-                    System.out.println("Found user: " + id + " | " +  firstName + " | " + lastName + " | " + registrationDate);
+                    System.out.println("Found user: " + id + " | " + firstName +
+                            " | " + lastName + " | " + registrationDate);
                 }
             }
         }
@@ -33,7 +36,8 @@ public class ApplicationV10 {
 
     private static DataSource createDataSource() {
         HikariDataSource ds = new HikariDataSource();
-        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema" +
+                ".sql'");
         ds.setUsername("sa");
         ds.setPassword("s3cr3tPassword");
         return ds;
