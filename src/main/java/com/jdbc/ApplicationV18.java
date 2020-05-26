ba41da1d59da51fe391e851cc0f09145a0ae525b
marco <marco@marcobehler.com>
2020-05-26 11:56:23 +0200

    Application V18

---
+++ src/main/java/com/jdbc/ApplicationV18.java
@@ -0,0 +1,128 @@
+package com.jdbc;
+
+import com.zaxxer.hikari.HikariDataSource;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class ApplicationV18 {
+
+    private static DataSource ds = createDataSource();
+
+    public static void main(String[] args) throws SQLException {
+        // database locking
+
+        int senderId = createUser();
+        int receiverId = createUser();
+
+        Connection connection = ds.getConnection();
+        try (connection) {
+            connection.setAutoCommit(false);
+            int transactionId = sendMoney(connection, senderId, receiverId, 50);
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+        }
+
+        Connection connection2 = ds.getConnection();
+        try (connection2) {
+            connection2.setAutoCommit(false);
+            int transactionId = sendMoney(connection2, senderId, receiverId, 19);
+            connection2.commit();
+        } catch (SQLException e) {
+            connection2.rollback();
+        }
+
+        int senderBalance = getBalance(senderId);
+        System.out.println("senderBalance = " + senderBalance);
+    }
+
+    private static Integer getBalance(int userId) throws SQLException {
+        Connection connection = ds.getConnection();
+        Integer balance = null;
+
+        try (connection; PreparedStatement stmt = connection.prepareStatement(
+                "select balance" +
+                " " +
+                "from users where id = ?")) {
+
+            stmt.setInt(1, userId);
+
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                balance = resultSet.getInt("balance");
+                break;
+            }
+        }
+        return balance;
+    }
+
+
+    private static int createUser() throws SQLException {
+        Connection connection = ds.getConnection();
+
+        try (PreparedStatement stmt = connection.prepareStatement("insert into " +
+                        "users (first_name, last_name, registration_date) values " +
+                        "(?,?,?)"
+                , Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, "[Some FirstName]");
+            stmt.setString(2, "[Some LastName]");
+            stmt.setObject(3, LocalDateTime.now());
+            stmt.executeUpdate();
+
+            final ResultSet keysResultSet = stmt.getGeneratedKeys();
+            keysResultSet.next();
+            return keysResultSet.getInt(1);
+        }
+    }
+
+    private static int sendMoney(Connection connection, int senderId,
+                                 int receiverId, int amount) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement("update users " +
+                "set balance = (balance - ?) where id = ?")) {
+            stmt.setInt(1, amount);
+            stmt.setInt(2, senderId);
+            stmt.executeUpdate();
+        }
+
+        try (PreparedStatement stmt = connection.prepareStatement("update users " +
+                "set balance = (balance + ?) where id = ?")) {
+            stmt.setInt(1, amount);
+            stmt.setInt(2, receiverId);
+            stmt.executeUpdate();
+        }
+
+        try (PreparedStatement stmt = connection.prepareStatement("insert into " +
+                        "transactions (sender, receiver, amount) values (?,?,?)"
+                , Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, senderId);
+            stmt.setInt(2, receiverId);
+            stmt.setInt(3, amount);
+            stmt.executeUpdate();
+
+            final ResultSet keysResultSet = stmt.getGeneratedKeys();
+            keysResultSet.next();
+            return keysResultSet.getInt(1);
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema" +
+                ".sql'");
+        ds.setUsername("sa");
+        ds.setPassword("s3cr3tPassword");
+
+        DataSource dataSource =
+                ProxyDataSourceBuilder.create(ds)  // pass original datasource
+                        .logQueryToSysOut()
+                        // .logQueryByJUL()
+                        //.logQueryBySlf4j()
+                        //.logQueryByCommons()
+                        .build();
+
+        return dataSource;
+    }
+}
