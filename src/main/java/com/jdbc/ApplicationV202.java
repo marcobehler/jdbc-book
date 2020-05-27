9bcfb96b0e1da3c0893418df8c05edc11e479e65
marco <marco@marcobehler.com>
2020-05-27 12:17:48 +0200

    ApplicationV202

---
+++ src/main/java/com/jdbc/ApplicationV202.java
@@ -0,0 +1,152 @@
+package com.jdbc;
+
+import com.zaxxer.hikari.HikariDataSource;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class ApplicationV202 {
+
+    public static void main(String[] args) throws SQLException {
+        // read uncommitted
+
+        DataSource ds = createDataSource();
+
+        int senderId = -1;
+        int receiverId = -1;
+
+        Connection connection1 = ds.getConnection();
+        try (connection1) {
+            connection1.setAutoCommit(false);
+
+            senderId = createUser(connection1);
+            receiverId = createUser(connection1);
+
+            connection1.commit();
+        } catch (SQLException e) {
+            connection1.rollback();
+        }
+
+
+        Connection connection2 = ds.getConnection();
+
+        try (connection2) {
+            connection2.setAutoCommit(false);
+
+            Connection connection3 = ds.getConnection();
+            try (connection3) {
+                connection3.setAutoCommit(false);
+                connection3.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
+
+                Integer connection3BalanceBefore = getBalance(connection3, senderId);
+                System.out.println("connection3BalanceBefore = "
+                        + connection3BalanceBefore);
+
+                try (PreparedStatement stmt = connection2.prepareStatement(
+                        "update users set balance = (balance - ?) where id = ?")) {
+                    stmt.setInt(1, 99);
+                    stmt.setInt(2, senderId);
+                    stmt.executeUpdate();
+                }
+                // TODO update into receiver's balance
+                // TODO insert into transactions table
+
+                connection2.commit();
+
+                Integer connection3BalanceAfter = getBalance(connection3, senderId);
+                System.out.println("connection3BalanceAfter = "
+                        + connection3BalanceAfter);
+                connection3.commit();
+            }
+
+
+        } catch (SQLException e) {
+            connection2.rollback();
+        }
+    }
+
+    private static int createUser(Connection connection) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement("insert into " +
+                        "users (first_name, last_name, registration_date) values " +
+                        "(?,?,?)"
+                , Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, "[Some FirstName]");
+            stmt.setString(2, "[Some LastName]");
+            stmt.setObject(3, LocalDateTime.now());
+            stmt.executeUpdate();
+
+            final ResultSet keysResultSet = stmt.getGeneratedKeys();
+            keysResultSet.next();
+            return keysResultSet.getInt(1);
+        }
+    }
+
+    private static int sendMoney(Connection connection, int senderId,
+                                 int receiverId, int amount) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement("update users " +
+                "set balance = (balance - ?) where id = ?")) {
+            stmt.setInt(1, amount);
+            stmt.setInt(2, senderId);
+            stmt.executeUpdate();
+        }
+
+        try (PreparedStatement stmt = connection.prepareStatement("update users " +
+                "set balance = (balance + ?) where id = ?")) {
+            stmt.setInt(1, amount);
+            stmt.setInt(2, receiverId);
+            stmt.executeUpdate();
+        }
+
+        try (PreparedStatement stmt = connection.prepareStatement("insert into " +
+                        "transactions (sender, receiver, amount) values (?,?,?)"
+                , Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, senderId);
+            stmt.setInt(2, receiverId);
+            stmt.setInt(3, amount);
+            stmt.executeUpdate();
+
+            final ResultSet keysResultSet = stmt.getGeneratedKeys();
+            keysResultSet.next();
+            return keysResultSet.getInt(1);
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema" +
+                ".sql'");
+        ds.setUsername("sa");
+        ds.setPassword("s3cr3tPassword");
+
+        DataSource dataSource =
+                ProxyDataSourceBuilder.create(ds)  // pass original datasource
+                        .logQueryToSysOut()
+                        // .logQueryByJUL()
+                        //.logQueryBySlf4j()
+                        //.logQueryByCommons()
+                        .build();
+
+        return dataSource;
+    }
+
+    private static Integer getBalance(Connection connection, int userId) throws SQLException {
+        Integer balance = null;
+
+        try (PreparedStatement stmt = connection.prepareStatement(
+                "select balance" +
+                        " " +
+                        "from users where id = ?")) {
+
+            stmt.setInt(1, userId);
+
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                balance = resultSet.getInt("balance");
+                break;
+            }
+        }
+        return balance;
+    }
+}
